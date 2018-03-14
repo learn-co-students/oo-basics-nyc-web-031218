49538a158661e7d36ea94ba3b41fe9c973f4260c
laura-campbell <campbell.laura.elizabeth@gmail.com>
2018-03-14 10:10:58 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,10 @@
+class Book
+ def initialize(title)
+   @title = title
+ end
 
-
+def turn_page
+  puts "Flipping the page...wow, you read fast!"
+end
+attr_accessor :title, :author, :genre, :page_count
+end
