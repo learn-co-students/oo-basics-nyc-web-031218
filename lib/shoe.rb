107b20479c1b5adec1413c90d1db901f3ebe6ebf
Helen Liutongco <liutongcoh@gmail.com>
2018-03-14 10:44:42 -0400

    Add shoe.rb and book.rb

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+
+  def initialize(brand_name)
+    @brand_name = brand_name
+  end
+
+  def brand
+    @brand_name
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+end
