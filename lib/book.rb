b45dd3875b50abbe9677fbe68fa9fa21fbedfc94
rogercodes1 <roger.ucsb@gmail.com>
2018-03-14 19:20:33 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,22 @@
+class Book
+
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+
+  def initialize(title)
+    @title = title
+    @author = author
+  end
 
 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+
+end
+
+
+
+title = Book.new("And Then There Were None")
+author = Book.new ("Agatha Christie")
